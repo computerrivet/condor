a33af85e67608926bf7c36586231a8229205d9dc
Wade Johnson <wjohnson@thedrillbook.com>
2015-11-25 21:34:36 -0800

    Removed dup method

--- Condor.Core/EmailSender.cs
+++ Condor.Core/EmailSender.cs
@@ -8,48 +8,17 @@ using System.Net.Mail;
 using STrace = System.Diagnostics.Trace;
 
 using SendGrid;
-
 using RazorTemplates.Core;
 
 namespace Condor.Core
 {
-    /// <summary>
-    /// Project specific email sender
-    /// </summary>
-    public class EmailSender
+    public static class EmailSender
     {
-        public static void Send<TModel>(string templateName, string toEmailAddress, string firstName, string lastName, string subject, TModel model)
+        public static void Send<TModel>(string templateName, string toEmailAddress, string name, string subject, TModel model)
         {
-            // Create the email object first, then add the properties.
-            var mail = new SendGridMessage();
-
-            mail.From = new MailAddress(ConfigurationManager.AppSettings["Condor_Address"], 
-                        ConfigurationManager.AppSettings["Condor_Address_DisplayName"]);
-
-            // Create credentials, specifying your user name and password.
-            var credentials = new NetworkCredential(ConfigurationManager.AppSettings["Condor_Username"], 
-                        ConfigurationManager.AppSettings["Condor_Password"]);
-
-            // Create an SMTP transport for sending email.
-            var transportWeb = new Web(credentials);
-
-            //recipient address
-            mail.AddTo(string.Format("{0} {1} <{2}>", firstName.Replace("\"", ""), lastName.Replace("\"", ""), toEmailAddress));
-            mail.Subject = subject;
-
-            var html_template_name = templateName + "_html.cshtml";
-            var txt_template_name = templateName + "_txt.cshtml";
-
-            // check the cache and store it if not found
-            var html_template = GetTemplate<TModel>(html_template_name);
-
-            mail.Html = EmailTemplateCompiler.Merge<TModel>(html_template, model);
-
-            var txt_template = GetTemplate<TModel>(txt_template_name);
-
-            mail.Text = EmailTemplateCompiler.Merge<TModel>(txt_template, model);
-
-            transportWeb.DeliverAsync(mail).Wait();
+            var toEmailAddresses = new string[1] { toEmailAddress };
+            var names = new string[1] { name };
+            Send<TModel>(templateName, toEmailAddresses, names, subject, model);
         }
 
         public static void Send<TModel>(string templateName, string[] emailAddresses, string[] names, string subject, TModel model)
