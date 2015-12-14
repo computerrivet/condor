8b98b273acedd339a3592daf1b3a4d72ae796c6c
Wade Johnson <computerrivet@gmail.com>
2015-12-14 11:28:57 -0800

    Updated to Visual Studio 2015 Added Nunit Test Project

---
+++ Condor.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Condor.Tests
+{
+   [TestFixture]
+   public class SendEmailTest
+    {
+       
+        public void SendTestEmail()
+        {
+
+        }
+
+    }
+}
